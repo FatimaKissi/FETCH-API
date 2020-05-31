a666c50586871df0c618c9afb1696e45edc46fd6
fatima <kissitraore@outlook.com>
2020-05-31 22:01:14 +0100

    VS Iteration

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall')
-    .then(response => response.json())
-    .then(json => console.log(json));
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => appendData(data))
+
+if (localStorage.getItem('test') == "key") {
+  console.log("It's there!")
+} else {
+  localStorage.setItem('test', 'key')
+}
+function appendData(data) {
+    var mainContainer = document.getElementById("wallItems");
+    for (var i = 0; i < data.length; i++) {
+    }
+}

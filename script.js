607d6bdf4f1c8364356d22d705359651c326f075
fatima <kissitraore@outlook.com>
2020-06-01 15:28:49 +0100

    js update

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(
-    response => response.json()).then(data => console.log(data)
-)
-function appendData(wallItems) {
-    wallItems.forEach(function(element){
-        let newLi=document.createElement("LI");
-        let newContent = document.createTextNode(element);
-        newLi.appendChild(newContent);
-        let currentUl=document.getElementById("result");
-        document.body.insertBefore(newLi,currentUl)  
-        })
-}
+fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => addElements(data))
+function addElements(list){
+    list.forEach(function(element){
+    let newLi=document.createElement("li");
+    let newContent = document.createTextNode(element);
+    newLi.appendChild(newContent)
+    let currentUl=document.getElementById("result");
+    document.body.insertBefore(newLi,currentUl)  
+    })
+  }
